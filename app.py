d1269b8adbfe58c4c41084e2398447aced784c98
rcfn <ricardocamposfn@gmail.com>
2023-03-27 16:30:14 -0300

    30com

--- app.py
+++ app.py
@@ -11,16 +11,17 @@ def index():
 
 @app.route('/search', methods=['POST'])
 def search():
-    location = request.form['location']
+    latitude = request.form['latitude']
+    longitude = request.form['longitude']
     radius = request.form['radius']
     keyword = request.form['keyword']
     url = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
     params = {
         'query': keyword,
-        'location': location,
+        'location': f'{latitude},{longitude}',
         'radius': radius,
         'key': API_KEY,
-        'region': 'br'
+        'fields': 'formatted_address,name,rating,formatted_phone_number'
     }
     response = requests.get(url, params=params)
     results = response.json()['results']
@@ -29,10 +30,12 @@ def search():
         name = result['name']
         address = result['formatted_address']
         rating = result.get('rating', None)
+        phone = result.get('formatted_phone_number', None)
         business = {
             'name': name,
             'address': address,
-            'rating': rating
+            'rating': rating,
+            'phone': result.get('formatted_phone_number', None)
         }
         businesses.append(business)
     sorted_businesses = sorted(businesses, key=lambda x: x['rating'] or 0, reverse=True)
